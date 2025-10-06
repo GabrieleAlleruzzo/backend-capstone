19e7cc4e293313187ff8bc1312fe6fc00a787b36
Gabriele Alleruzzo <alleruzzo.gabriele.96@gmail.com>
2025-10-06 17:07:53 +0200

    Update WebConfig.java

--- src/main/java/Capstone/backend/Security/WebConfig.java
+++ src/main/java/Capstone/backend/Security/WebConfig.java
@@ -2,6 +2,7 @@ package Capstone.backend.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,7 +16,7 @@ public class WebConfig {
                 registry.addMapping("/**")
                         // "*" Permetti tutti gli IP/origini
                         // Ho impostato il mio URL
-                        .allowedOrigins("https://gabrielealleruzzocapstone.vercel.app/")
+                        .allowedOrigins("https://gabrielealleruzzocapstone.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
